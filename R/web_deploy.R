9aeba118057e0f47c52cb40277a7c5ea28552233
tomoyukif <f.tomoyuki@okayama-u.ac.jp>
2024-04-01 15:21:47 +0900

    web dploy

--- R/web_deploy.R
+++ R/web_deploy.R
@@ -3,15 +3,15 @@ library(rmarkdown)
 
 prev_fn <- read.table("latest_publist")
 
-# write.table(prev_fn, "previous_publist",
-            # row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(prev_fn, "previous_publist",
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 # Generate the latest publication list
 new_fn <- getIPSRpublist(out_dir = "publist_archive")
 
 # Save the latest publication list file name
-# write.table(new_fn, "latest_publist",
-            # row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(new_fn, "latest_publist",
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 render_site()
 
